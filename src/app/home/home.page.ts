bdd9ae33e1f521adad6040f30cd2db45fb905689
israel <israel.bejarano.a@gmail.com>
2019-11-03 14:23:45 +0100

    mostrar/ocultar login logout

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  isLogin = false;
 
   constructor() {}
+
+  login() {
+    console.log('login click');
+    this.isLogin = true;
+  }
+
+  logout() {
+    console.log('logout click');
+    this.isLogin = false;
+  }
 
 }

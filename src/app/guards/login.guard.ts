bdea4dc71e51608399418eed0534b5ff0e1a6790
israel <israel.bejarano.a@gmail.com>
2019-11-05 23:25:51 +0100

    implementacion basica del guard

--- src/app/guards/login.guard.ts
+++ src/app/guards/login.guard.ts
@@ -1,10 +1,18 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, CanLoad } from '@angular/router';
 import { Observable } from 'rxjs';
+import { UsuarioService } from '../services/usuario.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class LoginGuard implements  {
-  
+export class LoginGuard implements  CanLoad {
+
+  constructor(private usuarioService: UsuarioService) {
+
+  }
+
+  canLoad(): Observable<boolean> | Promise<boolean> | boolean {
+    return true;
+  }
 }

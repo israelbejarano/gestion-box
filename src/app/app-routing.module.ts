f527c24841b2fce0b49ee1efc9a8460659a6ca81
israel <israel.bejarano.a@gmail.com>
2019-11-06 22:50:10 +0100

    solved error ruta

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,10 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: '**', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
   { path: 'login', loadChildren: './pages/login/login.module#LoginPageModule' },
   { path: 'perfil-atleta', loadChildren: './pages/perfil-atleta/perfil-atleta.module#PerfilAtletaPageModule' },
+  { path: '**', redirectTo: 'home', pathMatch: 'full' }
 ];
 
 @NgModule({

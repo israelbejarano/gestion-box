5a5f52c730a27846a433b227ca989564abc4a120
israel <israel.bejarano.a@gmail.com>
2019-11-09 17:20:32 +0100

    add forms module

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { UsuarioBenchmarkComponent } from './usuario-benchmark/usuario-benchmark.component';
+import { FormsModule } from '@angular/forms';
 
 
 
@@ -9,7 +10,8 @@ import { UsuarioBenchmarkComponent } from './usuario-benchmark/usuario-benchmark
   declarations: [UsuarioBenchmarkComponent],
   imports: [
     CommonModule,
-    IonicModule
+    IonicModule,
+    FormsModule
   ],
   entryComponents:[UsuarioBenchmarkComponent],
   exports: [

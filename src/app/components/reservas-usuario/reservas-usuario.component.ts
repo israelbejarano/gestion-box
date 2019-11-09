c0c7838f1021b60dfd281c90fc608fdb82277c54
israel <israel.bejarano.a@gmail.com>
2019-11-10 00:20:44 +0100

    componente reservas usuario

---
+++ src/app/components/reservas-usuario/reservas-usuario.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reservas-usuario',
+  templateUrl: './reservas-usuario.component.html',
+  styleUrls: ['./reservas-usuario.component.scss'],
+})
+export class ReservasUsuarioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
